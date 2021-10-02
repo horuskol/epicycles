a00661d5bf9a7188c8ff9486d9d6d45b5da60f3d
Stuart Jones <stuart@savvywombat.com.au>
2021-10-02 10:53:07 +0930

    epicycles

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,33 @@
          xmlns="http://www.w3.org/2000/svg"
     >
         <circle cx="500" cy="500" r="400" fill="none" stroke="black"/>
+
+        <disc px="500" py="500" pr="400" r="200" :a="angle" color="red"/>
+        <disc px="500" py="500" pr="400" r="100" :a="angle" color="green"/>
+        <disc px="500" py="500" pr="400" r="50" :a="angle" color="blue"/>
     </svg>
 </template>
 
 <script>
+import Disc from './components/disc.svg'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    disc: Disc,
+  },
+  data() {
+    return {
+      angle: 0,
+      timer: null,
+    }
+  },
+  mounted() {
+    this.timer = setInterval(() => {
+      this.angle++;
+      if (this.angle === 360) {
+        this.angle = 0;
+      }
+    }, 50)
+  }
 }
 </script>
---
+++ src/components/disc.svg.vue
@@ -0,0 +1,51 @@
+<template>
+    <g fill="none" :stroke="color" :transform="`translate(${origin.x}, ${origin.y}) rotate(${rotate}, ${r}, ${pr}) `">
+        <g :transform="`rotate(${-a}, ${center.x}, ${center.y})`">
+            <line :x1="center.x" :x2="center.x" y1="0" :y2="center.y"/>
+            <circle :cx="r" :cy="r" :r="r" fill="none"/>
+        </g>
+    </g>
+</template>
+
+<script>
+export default {
+  name: 'Disc',
+  props: {
+    px: {
+      type: Number,
+    },
+    py: {
+      type: Number,
+    },
+    pr: {
+      type: Number,
+    },
+    r: {
+      type: Number,
+    },
+    a: {
+      type: Number,
+    },
+    color: {
+      type: String,
+    }
+  },
+  computed: {
+    center() {
+      return {
+        x: this.r,
+        y: this.r,
+      }
+    },
+    origin() {
+      return {
+        x: this.px - this.r,
+        y: this.py - this.pr,
+      }
+    },
+    rotate() {
+      return this.a * (this.pr / this.r);
+    }
+  }
+}
+</script>
