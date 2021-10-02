e2894147fee39e49e152b319e408d0945ee382d0
Stuart Jones <stuart@savvywombat.com.au>
2021-10-02 09:37:24 +0930

    removed boilerplate vue project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <svg viewBox="0 0 1000 1000"
+         xmlns="http://www.w3.org/2000/svg"
+    >
+        <circle cx="500" cy="500" r="400" fill="none" stroke="black"/>
+    </svg>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
