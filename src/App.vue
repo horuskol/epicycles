31b7a2818a9be72556751157b851a67a723806f8
Stuart Jones <stuart@savvywombat.com.au>
2021-10-06 14:24:59 +1030

    remove max angle behaviour - smoother rendering

--- src/App.vue
+++ src/App.vue
@@ -18,18 +18,22 @@
 
         <div class="grid-in-controls flex flex-col">
             <button class="border border-blue-500 rounded"
-                    @click.prevent="showCircles = !showCircles">Show Circles</button>
+                    @click.prevent="showCircles = !showCircles">Show Circles
+            </button>
 
             <button v-if="timer"
                     class="border border-blue-500 rounded"
-                    @click.prevent="pause">Pause</button>
+                    @click.prevent="pause">Pause
+            </button>
 
             <button v-if="!timer"
                     class="border border-blue-500 rounded"
-                    @click.prevent="start">Start</button>
+                    @click.prevent="start">Start
+            </button>
 
             <button class="border border-blue-500 rounded"
-                    @click.prevent="reset">Reset</button>
+                    @click.prevent="reset">Reset
+            </button>
         </div>
     </div>
 </template>
@@ -50,8 +54,13 @@ export default {
       radius: 400,
       timer: null,
       discs: [
-        {r: 200, center: {x: 0, y: 0}, p: {x: 0, y: 0}, l: 50, color: [255, 0, 0]},
-        {r: 100, center: {x: 0, y: 0}, p: {x: 0, y: 0}, l: 50, color: [255, 0, 191], parent:0},
+        {r: 400, center: {x: 0, y: 0}, p: {x: 0, y: 0}, l: 400, a: 0, color: [255, 255, 255]},
+        {r: 200, center: {x: 0, y: 0}, p: {x: 0, y: 0}, l: 200, a: 0, color: [255, 0, 0]},
+        {r: 200, center: {x: 0, y: 0}, p: {x: 0, y: 0}, l: 200, a: 0, color: [0, 255, 0], parent: 0},
+        {r: 200, center: {x: 0, y: 0}, p: {x: 0, y: 0}, l: 200, a: 0, color: [255, 0, 255], parent: 1},
+        {r: 200, center: {x: 0, y: 0}, p: {x: 0, y: 0}, l: 200, a: 0, color: [0, 255, 255], parent: 2},
+        {r: 200, center: {x: 0, y: 0}, p: {x: 0, y: 0}, l: 100, a: 0, color: [127, 0, 255], parent: 3},
+        {r: 200, center: {x: 0, y: 0}, p: {x: 0, y: 0}, l: 100, a: 0, color: [0, 127, 255], parent: 4},
       ],
       canvas: null,
       showCircles: true,
@@ -67,10 +76,6 @@ export default {
     this.$nextTick(() => {
       this.canvas = document.getElementById('epicycles').getContext('2d')
 
-      this.maxAngle = 360 * this.radius / Math.min(...this.discs.map((disc => {
-        return disc.radius
-      })))
-
       this.start()
     })
   },
@@ -83,8 +88,7 @@ export default {
 
       this.pause()
     },
-    pause()
-    {
+    pause() {
       clearInterval(this.timer)
       this.timer = null
     },
@@ -93,37 +97,35 @@ export default {
         this.calculate()
 
         this.angle += 1
-
-        if (this.angle >= this.maxAngle) {
-          this.angle = 0
-        }
       }, 1)
     },
     calculate() {
       this.discs.forEach((disc) => {
         if (disc.parent !== undefined) {
+          disc.a = - this.discs[disc.parent].a * disc.r / this.discs[disc.parent].r
+
           disc.center = {
-            x: this.discs[disc.parent].center.x + (this.discs[disc.parent].r - disc.r) * Math.cos(toRadians(this.angle) * disc.r / this.discs[disc.parent].r),
-            y: this.discs[disc.parent].center.y + (this.discs[disc.parent].r - disc.r) * Math.sin(toRadians(this.angle) * disc.r / this.discs[disc.parent].r),
+            x: this.discs[disc.parent].center.x + (this.discs[disc.parent].r - disc.r) * Math.cos(toRadians(this.discs[disc.parent].a) * disc.r / this.discs[disc.parent].r),
+            y: this.discs[disc.parent].center.y + (this.discs[disc.parent].r - disc.r) * Math.sin(toRadians(this.discs[disc.parent].a) * disc.r / this.discs[disc.parent].r),
           }
 
           disc.p = {
-            x: disc.center.x + disc.l * Math.cos((1 - disc.r / this.discs[disc.parent].r) * toRadians(this.angle)),
-            y: disc.center.y - disc.l * Math.sin((1 - disc.r / this.discs[disc.parent].r) * toRadians(this.angle)),
+            x: disc.center.x + disc.l * Math.cos(toRadians((1 - disc.r / this.discs[disc.parent].r) * this.discs[disc.parent].a)),
+            y: disc.center.y - disc.l * Math.sin(toRadians((1 - disc.r / this.discs[disc.parent].r) * this.discs[disc.parent].a)),
           }
         } else {
+          disc.a = - this.angle * disc.r / this.radius
+
           disc.center = {
             x: this.center.x + (this.radius - disc.r) * Math.cos(toRadians(this.angle) * disc.r / this.radius),
             y: this.center.y + (this.radius - disc.r) * Math.sin(toRadians(this.angle) * disc.r / this.radius),
           }
 
           disc.p = {
-            x: disc.center.x + disc.l * Math.cos((1 - disc.r / this.radius) * toRadians(this.angle)),
-            y: disc.center.y - disc.l * Math.sin((1 - disc.r / this.radius) * toRadians(this.angle)),
+            x: disc.center.x + disc.l * Math.cos(toRadians((1 - disc.r / this.radius) * this.angle)),
+            y: disc.center.y - disc.l * Math.sin(toRadians((1 - disc.r / this.radius) * this.angle)),
           }
         }
-
-
 
         if (disc.p.x > 0 && disc.p.x < 1000 && disc.p.y > 0 && disc.p.y < 1000) {
           const pixel = this.canvas.getImageData(Math.floor(disc.p.x / 2) - 1, Math.floor(disc.p.y / 2) - 1, 3, 3)
