3f4fcdec2abc0319abb90cd2d48c500140ceec91
Stuart Jones <stuart@savvywombat.com.au>
2021-10-06 11:34:46 +1030

    limit pixel changes to be within the canvas area

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,13 @@ export default {
       radius: 400,
       timer: null,
       discs: [
-        { r: 101, center: {x: 0, y: 0}, p: {x: 0, y: 0}, l: 133, color: [255, 0, 0] },
+        { r:  25, center: {x: 0, y: 0}, p: {x: 0, y: 0}, l: 375, color: [255,   0,   0] },
+        { r:  50, center: {x: 0, y: 0}, p: {x: 0, y: 0}, l: 350, color: [255, 127,   0] },
+        { r:  75, center: {x: 0, y: 0}, p: {x: 0, y: 0}, l: 325, color: [255, 255,   0] },
+        { r: 100, center: {x: 0, y: 0}, p: {x: 0, y: 0}, l: 300, color: [  0, 255,   0] },
+        { r: 125, center: {x: 0, y: 0}, p: {x: 0, y: 0}, l: 275, color: [  0,   0, 255] },
+        { r: 150, center: {x: 0, y: 0}, p: {x: 0, y: 0}, l: 250, color: [127,   0, 255] },
+        { r: 175, center: {x: 0, y: 0}, p: {x: 0, y: 0}, l: 225, color: [255,   0, 255] },
       ],
       canvas: null,
     }
@@ -57,15 +63,17 @@ export default {
             y: disc.center.y - disc.l * Math.sin((1 - disc.r / this.radius) * toRadians(this.angle)),
           }
 
-          const pixel = this.canvas.getImageData(Math.floor(disc.p.x / 2) - 1, Math.floor(disc.p.y / 2) - 1, 3, 3)
-          for (let i = 0; i < 36; i += 4) {
-            pixel.data[i] = disc.color[0]
-            pixel.data[i + 1] = disc.color[1]
-            pixel.data[i + 2] = disc.color[2]
-            pixel.data[i + 3] = 127
-          }
+          if (disc.p.x > 0 && disc.p.x < 1000 && disc.p.y > 0 && disc.p.y < 1000) {
+            const pixel = this.canvas.getImageData(Math.floor(disc.p.x / 2) - 1, Math.floor(disc.p.y / 2) - 1, 3, 3)
+            for (let i = 0; i < 36; i += 4) {
+              pixel.data[i] = disc.color[0]
+              pixel.data[i + 1] = disc.color[1]
+              pixel.data[i + 2] = disc.color[2]
+              pixel.data[i + 3] = 127
+            }
 
-          this.canvas.putImageData(pixel, Math.floor(disc.p.x / 2) - 1, Math.floor(disc.p.y / 2) - 1)
+            this.canvas.putImageData(pixel, Math.floor(disc.p.x / 2) - 1, Math.floor(disc.p.y / 2) - 1)
+          }
         })
 
         this.angle += 1;
